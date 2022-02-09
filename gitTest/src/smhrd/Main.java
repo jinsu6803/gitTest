57a1987d24155960e580e37774c9dc1de716e8c4
SM210709 <SM210709@121.179.83.53>
2022-02-10 04:09:22 +0900

    안녕하세요 기능 추가!

--- gitTest/src/smhrd/Main.java
+++ gitTest/src/smhrd/Main.java
@@ -5,6 +5,9 @@ public class Main {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Hello World");
+		int num = 5;
+		int num1 = 10;
+		System.out.println(2+(num + num1)/2);
 	}
 
 }
